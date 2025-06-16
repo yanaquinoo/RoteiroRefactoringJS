d52c36b780fde64027f9b1b916e03860c72088f1
yanaquinoo <yanaquino88@gmail.com>
2025-06-15 22:55:08 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr(fatura, pecas) {
-  // função extraída
-  function calcularTotalApresentacao(apre, peca) {
+  // função query
+  function getPeca(apre) {
+    return pecas[apre.id];
+  }
+
+  // cálculo extraído sem parâmetro 'peca'
+  function calcularTotalApresentacao(apre) {
     let total = 0;
-    switch (peca.tipo) {
+    switch (getPeca(apre).tipo) {
       case "tragedia":
         total = 40000;
         if (apre.audiencia > 30) {
@@ -20,30 +25,30 @@ function gerarFaturaStr(fatura, pecas) {
         total += 300 * apre.audiencia;
         break;
       default:
-        throw new Error(`Peça desconhecida: ${peca.tipo}`);
+        throw new Error(`Peça desconhecida: ${getPeca(apre).tipo}`);
     }
     return total;
   }
 
   let totalFatura = 0;
-  let creditos = 0;
-  let faturaStr = `Fatura ${fatura.cliente}\n`;
-  const formato = new Intl.NumberFormat("pt-BR", {
+  let creditos    = 0;
+  let faturaStr   = `Fatura ${fatura.cliente}\n`;
+  const formato   = new Intl.NumberFormat("pt-BR", {
     style: "currency", currency: "BRL", minimumFractionDigits: 2
   }).format;
 
   for (let apre of fatura.apresentacoes) {
-    const peca = pecas[apre.id];
-    // agora chamamos a função extraída
-    let total = calcularTotalApresentacao(apre, peca);
+    // agora só chamamos com um parâmetro
+    let total = calcularTotalApresentacao(apre);
 
     // créditos para próximas contratações
     creditos += Math.max(apre.audiencia - 30, 0);
-    if (peca.tipo === "comedia")
+    if (getPeca(apre).tipo === "comedia") {
       creditos += Math.floor(apre.audiencia / 5);
+    }
 
-    // mais uma linha da fatura
-    faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+    // montagem da linha da fatura
+    faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
     totalFatura += total;
   }
 
@@ -53,5 +58,5 @@ function gerarFaturaStr(fatura, pecas) {
 }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
-const pecas   = JSON.parse(readFileSync('./pecas.json'));
+const pecas    = JSON.parse(readFileSync('./pecas.json'));
 console.log(gerarFaturaStr(faturas, pecas));
